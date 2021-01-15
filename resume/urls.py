ef68ddb1a3dd612a888cede0b5c4124553d6352a
Sahil <jainsahil260@gmail.com>
2021-01-15 15:21:41 +0530

    Update urls.py

--- resume/urls.py
+++ resume/urls.py
@@ -22,6 +22,7 @@ from project import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = 'index'),
+    path('project/<int:project_id>', views.detail, name = 'detail'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
