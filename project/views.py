746e7753fe1ca2b308a7ab2ad00733813c2bfdc7
Sahil <jainsahil260@gmail.com>
2021-01-15 15:20:24 +0530

    Added a detail function in views

--- project/views.py
+++ project/views.py
@@ -10,3 +10,11 @@ def index(request):
 		'projects':projects
 	}
 	return render(request, 'project/index.html',context)
+
+def detail(request, project_id):
+	"""Return the project details"""
+	project = get_object_or_404(Project, id = project_id)
+	context = {
+		'project': project
+	}
+	return render(request, 'project/detail.html', context)
