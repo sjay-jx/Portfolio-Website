d49f15ed8b66cc4a91d3721e25311559da13ac70
Sahil <jainsahil260@gmail.com>
2021-01-15 12:38:17 +0530

    Added context in index view

--- project/views.py
+++ project/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Project
 
 
 def index(request):
 	"""Return the homepage for the website"""
-	return render(request, 'project/index.html')
+	projects = Project.objects.all()
+	context = {
+		'projects':projects
+	}
+	return render(request, 'project/index.html',context)
