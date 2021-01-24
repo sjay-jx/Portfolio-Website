36d5f55aa8b7a089cc814b2328ccdefff61d2548
Sahil <jainsahil260@gmail.com>
2021-01-24 21:07:15 +0530

    Update Files

--- resume/urls.py
+++ resume/urls.py
@@ -19,12 +19,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 from project import views
 
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = 'index'),
     path('project/<int:project_id>', views.detail, name = 'detail'),
-
+]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
